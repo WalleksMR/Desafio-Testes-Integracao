1726eb43d9b5a2f71fd9e7d69321d54bbaef192d
WalleksMR <walleksmiranda@gmail.com>
2022-01-29 15:00:40 -0300

    test: should be able to create a new statement deposit

---
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -0,0 +1,47 @@
+import { Connection } from "typeorm";
+import createConnection from "../../../../database";
+import response from "supertest";
+import { app } from "../../../../app";
+
+describe("Create Statement Controller", () => {
+  const mokeUser = {
+    name: "user Moke",
+    email: "user@example.com",
+    password: "passwordMokeuser",
+  };
+  enum OperationType {
+    DEPOSIT = "deposit",
+    WITHDRAW = "withdraw",
+  }
+
+  let connection: Connection;
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new statement deposit", async () => {
+    const user = await response(app).post("/api/v1/users").send(mokeUser);
+    const token = await response(app)
+      .post("/api/v1/sessions")
+      .send({ email: mokeUser.email, password: mokeUser.password });
+    const statement = await response(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        user_id: String(user.body.id),
+        amount: 200,
+        description: "Create Statemnt Deposit",
+        type: OperationType.DEPOSIT,
+      })
+      .set({ Authorization: `Bearer ${token.body.token}` });
+
+    expect(statement.status).toBe(201);
+    expect(statement.body).toHaveProperty("id");
+    expect(statement.body).toHaveProperty("user_id");
+    expect(statement.body).toHaveProperty("type");
+  });
+});
