c3f6f8c845ae91d4470c0a12367927847d0d60be
WalleksMR <walleksmiranda@gmail.com>
2022-01-29 15:02:01 -0300

    test: should be able list all users but need is authenticated

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -0,0 +1,37 @@
+import response from "supertest";
+import { Connection } from "typeorm";
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+
+describe("Show User Profile Controller", () => {
+  let connection: Connection;
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  const userMoke = {
+    name: "user Controller",
+    email: "user@example.com",
+    password: "password",
+  };
+
+  it("should be able list all users but need is authenticated POST /api/v1/sessions", async () => {
+    await response(app).post("/api/v1/users").send(userMoke);
+    const user = await response(app)
+      .post("/api/v1/sessions")
+      .send({ email: userMoke.email, password: userMoke.password });
+
+    const listUser = await response(app)
+      .get("/api/v1/profile")
+      .set({ Authorization: `Bearer ${user.body.token}` });
+
+    expect(listUser.status).toBe(200);
+    expect(listUser.body).toHaveProperty("id");
+  });
+});
