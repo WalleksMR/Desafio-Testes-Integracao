b700276cbaf20b81a74800e154189e35e2001c2c
WalleksMR <walleksmiranda@gmail.com>
2022-01-29 15:08:58 -0300

    refactor: adjusted connection database

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import "express-async-errors";
 import express from "express";
 import cors from "cors";
 
-import "./database";
+import connectionDatabse from "./database";
 import "./shared/container";
 import { router } from "./routes";
 import { AppError } from "./shared/errors/AppError";
 
+connectionDatabse();
 const app = express();
 
 app.use(cors());
